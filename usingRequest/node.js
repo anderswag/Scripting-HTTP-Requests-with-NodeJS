04a4984276ac95516e1fc08df4299ef4dde8f7f2
Anderson Hung <anderson.hung@threepointturn.com>
2016-09-12 12:00:20 -0400

    first commit

---
+++ usingRequest/node.js
@@ -0,0 +1,19 @@
+var request = require("request");
+
+request("http://www.google.com", function(err, response, body) {
+
+  if (err) {
+    throw err;
+  }
+
+  console.log("Response Status Code:", response.statusCode);
+  printExampleHTML(body);
+});
+
+
+
+
+
+function printExampleHTML(callback) {
+  console.log(callback);
+}
